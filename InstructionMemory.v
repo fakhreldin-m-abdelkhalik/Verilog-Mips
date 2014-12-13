e3018762958088c934b92270222cb71808334e0b
Youssef-Emad <youssef.emad.attia@gmail.com>
2014-12-13 09:54:09 +0200

    Fix file path

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -2,17 +2,18 @@ module InstructionMemory(clock,PC,Instruction);
 	
 	input wire clock;
 	input wire [31:0] PC;
-	output reg [31:0] Instruction;
+	output wire [31:0] Instruction;
+	integer file;
 
 	parameter IMSize = 1024;
 
 	reg[7:0] InstructionMemory[0:IMSize];
 
 	initial begin
-		$readmemh("file",memory);
+		$readmemh("C:/Users/Youssef/Desktop/jil",InstructionMemory);
 	end
 	
-	assign Instruction <= {InstructionMemory[PC],InstructionMemory[PC+1],InstructionMemory[PC+2],InstructionMemory[PC+3]};	
+	assign Instruction = {InstructionMemory[PC],InstructionMemory[PC+1],InstructionMemory[PC+2],InstructionMemory[PC+3]};	
 
 endmodule
 
