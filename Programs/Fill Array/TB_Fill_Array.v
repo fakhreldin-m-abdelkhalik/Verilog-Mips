43d948b489d23cabbcfa6241392896a3d78550d9
fakhreldin-m-abdelkhalik <fakhreldin.m.abdelkhalik@gmail.com>
2014-12-14 18:34:18 +0200

    adjust TB_fill array and RegFile

--- Programs/Fill Array/TB_Fill_Array.v
+++ Programs/Fill Array/TB_Fill_Array.v
@@ -17,7 +17,7 @@ initial begin
 	Registers[24] = "$t8"; Registers[25] = "$t9"; Registers[26] = "$k0"; Registers[27] = "$k1";
 	Registers[28] = "$gp"; Registers[29] = "$sp"; Registers[30] = "$fp"; Registers[31] = "$ra";
 	
-	$readmemh("Instructions.txt", cpu.IM.InstructionMemory);
+	$readmemh("C:/Users/Fakhr El Din/Desktop/Verilog-Mips/Programs/Fill Array/Instructions.txt", cpu.IM.InstructionMemory);
 	
 	#760
 
