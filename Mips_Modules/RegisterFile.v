b22b4da05ad510235868c6c1b2e7f244c4698463
Youssef-Emad <youssef.emad.attia@gmail.com>
2014-12-18 13:59:59 +0200

    Edit RegisterFile code indentation

--- Mips_Modules/RegisterFile.v
+++ Mips_Modules/RegisterFile.v
@@ -1,45 +1,45 @@
 `timescale 1ps/1ps
 module RegisterFile (ReadData1 ,ReadData2 ,ReadRegister1 ,ReadRegister2 ,WriteRegister ,WriteData ,RegWrite ,clk);
 
-//outputs declaration
-output [31:0] ReadData1 ;
-wire [31:0] ReadData1 ;
-output [31:0] ReadData2 ;
-wire [31:0] ReadData2 ;
+	//outputs declaration
+	output [31:0] ReadData1 ;
+	wire [31:0] ReadData1 ;
+	output [31:0] ReadData2 ;
+	wire [31:0] ReadData2 ;
 
-//inputs declaration
-input [4:0] ReadRegister1 ;
-wire [4:0] ReadRegister1 ;
-input [4:0] ReadRegister2 ;
-wire [4:0] ReadRegister2 ;
-input [4:0] WriteRegister ;
-wire [4:0] WriteRegister ;
-input [31:0] WriteData ;
-wire [31:0] WriteData ;
-input RegWrite ;
-wire RegWrite ;	 
-input clk;
-wire clk;
+	//inputs declaration
+	input [4:0] ReadRegister1 ;
+	wire [4:0] ReadRegister1 ;
+	input [4:0] ReadRegister2 ;
+	wire [4:0] ReadRegister2 ;
+	input [4:0] WriteRegister ;
+	wire [4:0] WriteRegister ;
+	input [31:0] WriteData ;
+	wire [31:0] WriteData ;
+	input RegWrite ;
+	wire RegWrite ;	 
+	input clk;
+	wire clk;
 
-//inside module
-reg [31:0] Registers [31:0];
+	//inside module
+	reg [31:0] Registers [31:0];
 
-//Reading from registers
-assign #100  ReadData1=(ReadRegister1==5'b0) ? 32'b0 : Registers[ReadRegister1];
-assign #100  ReadData2=(ReadRegister2==5'b0) ? 32'b0 : Registers[ReadRegister2];
+	//Reading from registers
+	assign #100  ReadData1=(ReadRegister1==5'b0) ? 32'b0 : Registers[ReadRegister1];
+	assign #100  ReadData2=(ReadRegister2==5'b0) ? 32'b0 : Registers[ReadRegister2];
 
-//Writing in registers
-always @(posedge clk)	
-begin
-	#100 //delay 100ps for RF
-	if(RegWrite&&WriteRegister!=0) 
+	//Writing in registers
+	always @(posedge clk)	
+	begin
+		#100 //delay 100ps for RF
+		if(RegWrite&&WriteRegister!=0)
 			Registers[WriteRegister]<=WriteData;
-end
+	end
 
-initial
-begin
-	Registers[0]=0;
-	Registers[29]=1023;
-end
+	initial
+	begin
+		Registers[0]=0;
+		Registers[29]=1023;
+	end
 
 endmodule
