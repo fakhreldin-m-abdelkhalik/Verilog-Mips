4f6bcf617902fb7acbf56343f79f418240afb405
Youssef-Emad <youssef.emad.attia@gmail.com>
2014-12-18 13:56:44 +0200

    Edit PC code indentation

--- Mips_Modules/PC.v
+++ Mips_Modules/PC.v
@@ -1,13 +1,18 @@
 module PC(OUT,IN,clk);
+
 	input [31:0] IN;
 	input clk;
+
 	output [31:0] OUT;
+
 	wire [31:0] IN;
 	wire clk;	
 	reg [31:0] OUT;
+
 	always @(posedge clk)
 	begin
 		OUT<=IN;
 	end
+
 endmodule
 	
