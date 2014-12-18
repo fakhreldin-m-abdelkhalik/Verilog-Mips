c616716882a5c3870ceeddd0af1086c0be2163d0
Youssef-Emad <youssef.emad.attia@gmail.com>
2014-12-18 13:54:11 +0200

    Edit MUX_5 code indentation

--- Mips_Modules/MUX_5.v
+++ Mips_Modules/MUX_5.v
@@ -1,22 +1,23 @@
 module mux_5(out, s, in1, in2);
 
-input s;
-input [4:0] in1;
-input [4:0] in2;
+	input s;
+	input [4:0] in1;
+	input [4:0] in2;
 
-output [4:0] out;
+	output [4:0] out;
 
-wire s;
-wire [4:0] in1;
-wire [4:0] in2;
+	wire s;
+	wire [4:0] in1;
+	wire [4:0] in2;
 
-reg [4:0] out;
+	reg [4:0] out;
 
-always @ (s or in1 or in2) begin
-	
-	if (s)
-		out <= in2;
-	else
-		out <= in1;
-end
+	always @ (s or in1 or in2) begin
+		
+		if (s)
+			out <= in2;
+		else
+			out <= in1;
+	end
+
 endmodule
