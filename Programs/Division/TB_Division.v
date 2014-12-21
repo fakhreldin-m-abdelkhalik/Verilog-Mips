9792348070014e8b12badda3a7f10927e12d454c
Youssef-Emad <youssef.emad.attia@gmail.com>
2014-12-21 19:22:54 +0200

    edit Division delay

--- Programs/Division/TB_Division.v
+++ Programs/Division/TB_Division.v
@@ -20,7 +20,7 @@ initial begin
 	
 	$readmemh("Instructions.txt", cpu.IM.InstructionMemory);
 	
-	#62320
+	#24600
 
 	$display("Final value of PC = %h", cpu.ProgCounter.OUT);
 	for (i = 0; i < 32; i = i + 1) begin
